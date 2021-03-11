80647744825b8c3a96a9aef6b0c19861356f3835
breader124 <breaderman@gmail.com>
2021-03-12 00:34:19 +0100

    created config server using Spring Boot Config Server

---
+++ config-server/src/main/java/com/breader/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.breader.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
